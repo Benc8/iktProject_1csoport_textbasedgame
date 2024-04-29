6e4712fdb28da9f9290f01582d2a65b4b962c9fb
Benc8 <menyhert.bence08@gmail.com>
2024-04-29 20:08:33 +0200

    animations

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ function unHide(){
     document.getElementById("dots").style.display = "none";
     document.getElementById("hidden").style.display = "block";
     document.getElementById("hidden").classList.add("animate-slide-in");
+    document.getElementById("death").style.backgroundColor = "rgb(31, 0, 0)";
 };
