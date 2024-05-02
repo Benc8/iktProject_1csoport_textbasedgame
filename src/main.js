c5c22aa5c8e65ed3177656725038102730ee4a41
Benc8 <menyhert.bence08@gmail.com>
2024-05-02 11:54:36 +0200

    final

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+const lazyImage = document.getElementById('lazyImage');
+lazyImage.onload = function() {
+    lazyImage.style.opacity = 1;
+    lazyImage.classList.add('kep2');
+};
 
 function unHide(){
     document.getElementById("dots").style.display = "none";
     document.getElementById("hidden").style.display = "block";
     document.getElementById("hidden").classList.add("animate-slide-in");
-    document.getElementById("death").style.backgroundColor = "rgb(31, 0, 0)";
+    if(document.getElementById("death")){
+        document.getElementById("death").style.backgroundColor = "rgb(31, 0, 0)";
+    }
+    document.getElementById("asdf").style.backgroundColor = "rgb(0, 31, 0)";
 };
